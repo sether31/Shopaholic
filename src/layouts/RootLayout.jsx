d3db43f7dff2f5ee1c52b7cd677c21d449b48028
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-10-05 14:15:22 +0800

    style(nav): Add Icon on logo and style it

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
+import { Link, NavLink, Outlet } from 'react-router-dom'
 import { FaShoppingBag } from "react-icons/fa";
+import { TbNorthStar } from "react-icons/tb";
 
 
 export default function RootLayout() {
   return (
     <>
-      <header className='w-full text-white bg-black/90'>
+      <header className='fixed w-full text-white bg-black/90 z-1'>
         <nav className='flex items-center py-4 container-xl'>
-          <h1 className='mr-auto text-3xl font-bold'>Shopaholic</h1>
-            <NavLink className="px-4 py-2 text-lg" to="/">Home</NavLink>
-            <NavLink className="px-4 py-2 text-lg" to="shop">Shop</NavLink>
-            <NavLink className="px-4 py-2" to="cart">
-              <FaShoppingBag size={25} />
-            </NavLink>
+          {/* logo */}
+          <Link 
+            to="/"
+            className='flex items-center gap-2 mr-auto text-3xl font-bold'
+          >
+            <TbNorthStar/>
+            <span>Shopaholi</span>
+          </Link>
+          {/* links */}
+          <NavLink className="px-4 py-2 text-lg" to="/">Home</NavLink>
+          <NavLink className="px-4 py-2 text-lg" to="shop">Shop</NavLink>
+          <NavLink className="px-4 py-2" to="cart">
+            <FaShoppingBag size={25} />
+          </NavLink>
         </nav>
       </header>
 
-      <main className='mt-8 container-xl'>
+      <main>
         <Outlet />
       </main>
     </>
