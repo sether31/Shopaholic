62ed4dd6958e2161b388cecf6b35e212f2dfb99f
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-10-08 17:43:28 +0800

    feat(cart): Finish cart functionality
    - If cart is empty show the no cart function.
    - Automatically remove cart if quantity reaches zero.
    - Add remove button.
    - Add checkout button to checkout the carts and empty it.

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -35,13 +35,48 @@ export default function CartProvider({ children }) {
     });
   }
 
+  // update quantity
+  const updateQuantity = (id, quantity) => {
+    setCarts((prev) => {
+      if(quantity <= 0){
+        return prev.filter(item => item.id !== id)
+      } else {
+        return prev.map(item => {
+          return (item.id === id) ? {...item, quantity} : item
+        })
+      }
+    })
+  }
+
+  // remove item
+  const removeItem = (id) => {
+    setCarts((prev) => {
+      return prev.filter(item => item.id !== id);
+    })
+  }
+
+  // total amount
+  const totalAmount = () => {
+    const total = carts.reduce((total, item) => total += (item.price * item.quantity), 0)
+    return total;
+  }
+
+  // checkout all
+  const checkOut = () => {
+    const check = confirm('Are you sure you want to checkout your cart?');
+    if(check) {
+      alert('Checkout successful')
+      setCarts([]);
+    }
+  }
+
   const allQuantity = () => {
     const getQuantity = carts.reduce((total, item) => total += item.quantity, 0);
     return getQuantity;
   }
 
   return (
-    <CartContext.Provider value={{carts, addToCart, allQuantity}}>
+    <CartContext.Provider value={{carts, addToCart, allQuantity, updateQuantity, removeItem, totalAmount, checkOut}}>
       {children}
     </CartContext.Provider>
   )
